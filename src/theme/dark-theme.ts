c805df5afacaf8a70f8f0f74eb9c18a4d807c0d3
Draym <kevin.draym@gmail.com>
2023-04-02 14:57:52 +0800

    task: update dark theme

--- src/theme/dark-theme.ts
+++ src/theme/dark-theme.ts
@@ -9,5 +9,40 @@ export const darkTheme = createTheme({
         button: {
             textTransform: 'none'
         }
+    },
+    components: {
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    marginTop: "5px !important",
+                    marginBottom: "5px !important",
+                }
+            }
+        },
+        MuiFormControl: {
+            styleOverrides: {
+                root: {
+                    marginTop: "5px !important",
+                    marginBottom: "5px !important",
+                }
+            }
+        },
+        MuiInputLabel: {
+            styleOverrides: {
+                root: {
+                    transform: "translate(14px, 7px) scale(1)"
+                },
+                shrink: {
+                    transform: "translate(14px, -9px) scale(0.75)"
+                }
+            }
+        },
+        MuiInputBase: {
+            styleOverrides: {
+                input: {
+                    padding: "7px !important"
+                }
+            }
+        }
     }
 })
