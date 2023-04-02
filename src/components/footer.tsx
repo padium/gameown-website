d59d2f31ef8a26faeba010a2478e42a87d416ec4
Draym <kevin.draym@gmail.com>
2023-04-02 21:24:44 +0800

    task: update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,23 @@
-import {Container} from "@mui/material"
+import {Box, Container, Stack, Typography} from "@mui/material"
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import YouTubeIcon from '@mui/icons-material/YouTube'
+import FacebookIcon from '@mui/icons-material/Facebook'
 
 export default function Footer() {
     return (
-        <footer className="fixed bottom-0 w-full">
-            <Container>Hi im a footer</Container>
+        <footer className="fixed bottom-0 w-full" style={{borderTop: "0.1rem solid #DCDEDD"}}>
+            <Container>
+                <Stack direction="row" justifyContent="space-between" alignItems="center" height="50px">
+                    <Typography>© Padium, 2023 </Typography>
+                    <Box>
+                        <FacebookIcon/>
+                        <LinkedInIcon/>
+                        <TwitterIcon/>
+                        <YouTubeIcon/>
+                    </Box>
+                </Stack>
+            </Container>
         </footer>
     )
 }
