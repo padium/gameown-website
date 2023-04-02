51ea52a1df3afb3ce1c3a2d610f8be27a7949977
Draym <kevin.draym@gmail.com>
2023-04-02 21:46:02 +0800

    task: game profile add Tab menu

--- src/app/profiles/game/page.tsx
+++ src/app/profiles/game/page.tsx
@@ -2,7 +2,10 @@ import {useParams} from "react-router"
 import Profile from "../page"
 import {isNotEmpty, isNotNull} from "@d-lab/common-kit"
 import Loading from "../../../components/dashboard/loading"
-import {Box, Container, Grid} from "@mui/material"
+import {Box, Container, Grid, Tab, Tabs} from "@mui/material"
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import React, {useEffect, useState} from "react"
 import handleAuth from "../../../utils/auth/auth-refresh"
 import apiClient from "../../../clients/padium.client"
@@ -14,6 +17,7 @@ function GameProfilePage() {
     const {identifier} = useParams()
     const [loading, setLoading] = useState(true)
     const [game, setGame] = useState<GameDto>()
+    const [currentTab, setCurrentTab] = useState('1')
 
     useEffect(() => {
         if (isNotEmpty(identifier)) {
@@ -35,12 +39,30 @@ function GameProfilePage() {
     } else {
         menu = <GameProfileMenuWaitlist name={game!.name}/>
     }
+
+    const handleMenuChange = (event: React.SyntheticEvent, newValue: string) => {
+        setCurrentTab(newValue);
+    };
+
     return <Box sx={{backgroundColor: "#FBFBFA"}}>
         <GameProfileBanner bannerUrl={game!.bannerUrl}/>
         <Container sx={{marginTop: 1}}>
             <GameProfileHead game={game!} menu={menu}/>
             <Grid container>
-                <GameProfileDesc description={game!.description}/>
+                <Box sx={{ width: '100%', typography: 'body1' }}>
+                    <TabContext value={currentTab}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleMenuChange} aria-label="lab API tabs example">
+                                <Tab label="Description" value="1" />
+                                <Tab label="Community" value="2" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">
+                            <GameProfileDesc description={game!.description}/>
+                        </TabPanel>
+                        <TabPanel value="2">Community</TabPanel>
+                    </TabContext>
+                </Box>
             </Grid>
         </Container>
     </Box>
